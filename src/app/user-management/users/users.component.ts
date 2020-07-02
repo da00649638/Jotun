64fe69044c639b8934b40946794c9a4d254447bb
Annavarapu Nagendra Babu <AN00673231@techmahindra.com>
2020-07-02 12:41:05 +0530

    added json serve for sample local DB

--- src/app/user-management/users/users.component.ts
+++ src/app/user-management/users/users.component.ts
@@ -10,33 +10,35 @@ import { map, debounceTime, distinctUntilChanged, startWith } from 'rxjs/operato
 import { UsersServiceService } from '../../services/users-service.service';
 
 export interface PeriodicElement {
+  user_id: number;
+  user_group: string;
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  email_id: string;
+  role: string;
+  status: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  {user_id: 1, user_group: 'Norway Super User', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 2, user_group: 'Helium',  name: 'Donate12', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 3, user_group: 'Lithium', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 4, user_group: 'Beryllium', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 5, user_group: 'Boron', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 6, user_group: 'Carbon', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 7, user_group: 'Nitrogen', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 8, user_group: 'Oxygen', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 9, user_group: 'Fluorine', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 10, user_group: 'Neon', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 11, user_group: 'Sodium', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 12, user_group: 'Magnesium', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 13, user_group: 'Aluminum', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 14, user_group: 'Silicon', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 15, user_group: 'Phosphorus', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 16, user_group: 'Sulfur', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 17, user_group: 'Chlorine', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 18, user_group: 'Argon', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 19, user_group: 'Potassium', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
+  {user_id: 20, user_group: 'Calcium', name: 'Donate', email_id: 'donate@jotun.com', role: 'Super User', status: 'Active'},
 ];
 
 @Component({
